c6e212b3a5e507ed738a3ccdcf838331b7e8429f
MyoungSeob <woodyms@nate.com>
2023-06-06 00:39:04 +0900

    react-youtube & swiper 추가

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   env: {
     LOCAL_API_URL: process.env.LOCAL_API_URL,
     DEPLOY_API_URL: process.env.DEPLOY_API_URL,
+    LOCAL_URL: process.env.LOCAL_URL,
+    DEPLOY_URL: process.env.DEPLOY_URL,
   },
 };
 
