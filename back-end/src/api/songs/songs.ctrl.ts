522ceb703aee9e30c405be3f5fd209b2aba70197
Seobe <woodyms@nate.com>
2023-06-09 18:44:36 +0900

    노래 추천 알고리즘 추가

--- back-end/src/api/songs/songs.ctrl.ts
+++ back-end/src/api/songs/songs.ctrl.ts
@@ -1,4 +1,4 @@
-import { ParameterizedContext } from 'koa';
+import { Context, DefaultContext, ParameterizedContext } from 'koa';
 import Songs from '../../models/songs';
 
 const songsCtrl = {
@@ -18,7 +18,43 @@ const songsCtrl = {
         features,
       };
     } catch (e) {
-      console.log(e);
+      ctx.throw([500, e]);
+    }
+  },
+  recommend: async (ctx: ParameterizedContext) => {
+    try {
+      const { features } = ctx.request.body as { features: string[] };
+
+      const userFeaturesObject = features.reduce((obj, feature, index) => {
+        const key = feature;
+        const value = 5 - index;
+        obj[key] = value;
+        return obj;
+      }, {} as Record<string, number>);
+
+        console.log(userFeaturesObject)
+
+      const temp = await Songs.find({
+        features: { $in: features }
+      });
+
+
+      const recommendSongs = temp.map((song) => {
+        let score = 0;
+        
+        song.features.forEach((feature) => {
+          if (feature in userFeaturesObject) {
+            score += userFeaturesObject[feature];
+          }
+        });
+        
+        return { song, score };
+      });
+      
+      const result = recommendSongs.sort((a, b) => b.score - a.score).slice(0, 5);
+
+      ctx.body = { songs: result };
+    } catch (e) {
       ctx.throw([500, e]);
     }
   },
