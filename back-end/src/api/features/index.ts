b09151deb83fed60e9d0c9ce56517de1eb41a1b6
Seobe <woodyms@nate.com>
2023-06-13 23:16:21 +0900

    restructure

--- back-end/src/api/features/index.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-import featuresCtrl from './features.ctrl';
-
-const features = new Router();
-
-features.get('/', '');
-features.post('/', featuresCtrl.list);
-
-export default features;
